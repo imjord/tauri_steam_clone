2057b761bacc75a86200019156e3e6df67545696
imjord <dean.jay.goat@gmail.com>
2023-09-17 21:53:49 -0600

    added mongodb store

--- imjord_tauri_steam/server/server.js
+++ imjord_tauri_steam/server/server.js
@@ -8,7 +8,7 @@ const passport = require("passport");
 require("./utils/passport")(passport);
 const session = require("express-session");
 const routes = require("./routes/index");
-
+const MongoStore = require("connect-mongo");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
@@ -17,11 +17,21 @@ app.use(
     credentials: true,
   })
 );
+
+// session store db
+
+const options = {
+  mongoUrl: "mongodb://127.0.0.1:27017/imjord_tauri_steam",
+};
+
+const store = MongoStore.create(options);
+
 app.use(
   session({
     secret: "secret",
     saveUninitialized: true,
     resave: false,
+    store: store,
     cookie: {
       maxAge: 3600000,
     },
